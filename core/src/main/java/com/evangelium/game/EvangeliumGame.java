628ccf4488b3204e92d9867b8d3b1e3a8a544f3c
Dante <danielbedoyalopez5@gmail.com>
2026-04-22 22:05:19 -0500

    Mapa de la calle inicial guardado en assets

--- core/src/main/java/com/evangelium/game/EvangeliumGame.java
+++ core/src/main/java/com/evangelium/game/EvangeliumGame.java
@@ -1,32 +1,40 @@
 package com.evangelium.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class EvangeliumGame extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture image;
+    private OrthographicCamera camera;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        
+        // La cámara nos permite ver el mundo. 800x480 es una buena resolución base.
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, 800, 480);
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+        // Establecemos el color de fondo: Azul muy oscuro (R, G, B, Alpha)
+        Gdx.gl.glClearColor(0.02f, 0.02f, 0.1f, 1); 
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        camera.update();
+        batch.setProjectionMatrix(camera.combined);
+
         batch.begin();
-        batch.draw(image, 140, 210);
+        // Aquí es donde dibujaremos todo en el futuro
         batch.end();
     }
-
+    
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
     }
 }
